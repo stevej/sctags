10e1f8ef9a8313945f33fee84ad566cc52605353
Geoff Reedy <geoff@programmer-monk.net>
2008-01-20 13:36:29 -0800

    resolve etags conflicts
    darcs-hash:20080120213629-8d81a-e796270a048f7ffb61e2ce2f465e3a6073cfe5c6.gz

--- src/main/scala/net/pmonk/tools/sctags/TagGeneration.scala
+++ src/main/scala/net/pmonk/tools/sctags/TagGeneration.scala
@@ -10,33 +10,26 @@ trait TagGeneration { this: SCTags.type =>
   import compiler._
   object generateTags {
 
-  def apply[T <: Trees with Names](tree: T#Tree)(implicit compiler: T): Seq[Tag] = {
+    def apply[T <: Global](tree: Global#Tree): Seq[Tag] = {
 
-
-    class GenTagTraverser extends compiler.Traverser {
-      val _tags = new ListBuffer[Tag]
-      def tags: Seq[Tag] = _tags.toList
+      class GenTagTraverser extends compiler.Traverser {
+        val _tags = new ListBuffer[Tag]
+        def tags: Seq[Tag] = _tags.toList
 
         import compiler._;
 
-      def source(tree: Tree): Option[Tuple2[String,String]] = {
-        tree.pos.source match {
-          case Some(source) if tree.pos.lineContent != null =>
-          Some((source.path, "/^" + tree.pos.lineContent + "$/"));
-          case _ => None;
-        }
-      }
-
-      def tag_(file: String, address: String)(name: Name, kind: char, fields: Tuple2[String, String]*) = {
-        if (!name.decode.contains('$') && !name.decode.equals("this")) {
-          val withKind = Seq.singleton("kind"->kind.toString).projection.append(fields)
-          _tags += new Tag(name.decode, file, address, withKind: _*)
+        def tag_(pos: Position)(name: Name, kind: char, fields: Tuple2[String, String]*) = {
+          if (!name.decode.contains('$') && !name.decode.equals("this")) {
+            val withKind = Seq.singleton("kind"->kind.toString).projection.append(fields)
+            _tags += Tag(name.decode, pos, withKind: _*)
+          }
         }
 
-      override def traverse(t: Tree) {
-        source(t) match {
-          case Some((file, address)) =>
-            def tag = tag_(file, address)_
+        override def traverse(t: Tree) {
+          for (line <- t.pos.line;
+               col <- t.pos.column) {
+            val text = t.pos.lineContent
+            def tag = tag_(Position(line, col, text))_
             t match {
               case ModuleDef(mods, name, _) =>
                 tag(name, 'o');
@@ -58,14 +51,13 @@ trait TagGeneration { this: SCTags.type =>
                 tag(name, 't')
               case _ => ();
             }
-          case _ => ();
+          }
+          super.traverse(t)
         }
-        super.traverse(t)
       }
+      val traverser = new GenTagTraverser
+      traverser.traverse(tree.asInstanceOf[compiler.Tree])
+      traverser.tags
     }
-
-    val traverser = new GenTagTraverser
-    traverser.traverse(tree.asInstanceOf[compiler.Tree])
-    traverser.tags
   }
 }
