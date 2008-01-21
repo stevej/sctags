04b7ced6d8b6e7b4a0694f8f22602b508f8f9e7f
Geoff Reedy <geoff@programmer-monk.net>
2008-01-20 17:44:31 -0800

    refactor tag generation
    darcs-hash:20080121014431-8d81a-5ebd48bd7eaf4d486d6fd4fa90c390ad6d8faa86.gz

--- src/main/scala/net/pmonk/tools/sctags/TagGeneration.scala
+++ src/main/scala/net/pmonk/tools/sctags/TagGeneration.scala
@@ -1,24 +1,56 @@
 package net.pmonk.tools.sctags
 
 import scala.tools.nsc.Global;
-import scala.tools.nsc.ast.Trees
 import scala.tools.nsc.symtab.Names
 
-import scala.collection.mutable.ListBuffer
+import scala.collection._
 
 trait TagGeneration { this: SCTags.type =>
   import compiler._
   def generateTags(tree: Tree): Seq[Tag] = {
     class GenTagTraverser extends compiler.Traverser {
-      val _tags = new ListBuffer[Tag]
+      val _tags = new mutable.ListBuffer[Tag]
       def tags: Seq[Tag] = _tags.toList
 
       import compiler._;
 
-      def tag_(pos: Position)(name: Name, kind: char, fields: Tuple2[String, String]*) = {
+      def addTag(pos: Position, name: Name, fields: Map[String, String]) {
         if (!name.decode.contains('$') && !name.decode.equals("this")) {
-          val withKind = Seq.singleton("kind"->kind.toString).projection.append(fields)
-          _tags += Tag(name.decode, pos, withKind: _*)
+          _tags += Tag(name.decode, pos, fields.toList: _*)
+        }
+      }
+
+      def testMods[A](t: Tree, tests: Seq[(Modifiers => Boolean, A)]): Option[A] = {
+        t match {
+          case (mdef: MemberDef) => tests.find(_._1(mdef.mods)).map(_._2)
+          case _ => None
+        }
+      }
+
+      def access(t: Tree) = testMods(t,
+        List(
+          ((m: Modifiers) => m.isPrivate, "private"),
+          ((m: Modifiers) => m.isPublic, "public"),
+          ((m: Modifiers) => m.isProtected, "protected")))
+
+      def implementation(t: Tree) = testMods(t,
+        List(
+          ((m: Modifiers) => m.isFinal, "final"),
+          ((m: Modifiers) => m.isAbstract, "abstract"),
+          ((m: Modifiers) => m.isSealed, "sealed")))
+
+      def kind(t: Tree) = {
+        t match {
+          case ModuleDef(_,_,_) => Some("v")
+          case ClassDef(mods,_,_,_) => testMods(t,
+            List(
+              ((m: Modifiers) => m.isCase, "C"),
+              ((m: Modifiers) => m.isTrait, "T"),
+              ((m: Modifiers) => true, "c")))
+          case ValDef(_,_,_,_) => Some("v")
+          case DefDef(_,_,_,_,_,_) => Some("m")
+          case TypeDef(_,_,_,_) => Some("t")
+          case _ => None
         }
       }
 
@@ -26,27 +58,22 @@ trait TagGeneration { this: SCTags.type =>
         for (line <- t.pos.line;
              col <- t.pos.column) {
           val text = t.pos.lineContent
-          def tag = tag_(Position(line, col, text))_
-          t match {
-            case ModuleDef(mods, name, _) =>
-              tag(name, 'o');
-            case ClassDef(mods, name, tparams, _) => {
-              var kind = if (mods.isCase) {
-                'C'
-              } else if (mods.isTrait) {
-                'T'
-              } else {
-                'c'
-              }
-              tag(name, kind);
-            };
-            case ValDef(mods, name, _, _) =>
-              tag(name, 'v')
-            case DefDef(mods, name, _, _, _, _) =>
-              tag(name, 'm')
-            case TypeDef(mods, name, _, _) =>
-              tag(name, 't')
-            case _ => ();
+          var fields: immutable.Map[String, String] = immutable.Map.empty
+          def addField(name: String, getter: Tree => Option[String]) {
+            getter(t) match {
+              case Some(value) => fields += (name -> value)
+              case None => ()
+            }
+          }
+          addField("access", access(_))
+          addField("implementation", implementation(_))
+          addField("kind", kind(_))
+          val name = t match {
+            case (dtree: DefTree) => Some(dtree.name)
+            case _ => None
+          }
+          if (name.isDefined && fields.contains("kind")) {
+            addTag(Position(line, col, text), name.get, fields)
           }
         }
         super.traverse(t)
